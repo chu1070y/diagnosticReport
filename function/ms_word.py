f48ab0184a18fbba782d25e38134e809adb4751b
chu1070y <chu1070y@naver.com>
2025-04-24 14:21:46 +0900

    보고서 추가 내용 반영 완료

--- function/ms_word.py
+++ function/ms_word.py
@@ -216,120 +216,120 @@ class MSword(Common):
         # self.image_extensions = [".png", ".jpg", ".jpeg"]
 
         self.parser = Parser()
-
-    @Common.exception_handler
-    def make_report(self):
-        # 설정값 + 상태값 + 그래프 삽입
-        self.logger.info("input status and graph on diagnostic report")
-        db = DBwork()
-        status_data = db.get_latest_status_data()
-        os_info_data = dict()
-        variables_data = dict()
-
-        # 왼쪽 정렬 파라미터 리스트 (여기 없으면 모두 가운데 정렬)
-        left_aligned_param_list = [
-            'my.cnf', 'hostname', 'memory', 'os version', 'cpu', 'version',
-            'log_error', 'slow_query_log_file', 'log_bin_basename', 'innodb_data_file_path',
-            'default_storage_engine', 'port', 'socket', 'basedir', 'datadir'
-        ]
-
-        os_info_dict = self.get_os_info()
-        variables_data = parse_table_to_dict(os_info_dict.get('global variables'))
-
-        try:
-            os_info_dict = self.get_os_info()
-            variables_data = parse_table_to_dict(os_info_dict.get('global variables'))
-
-            os_info_data['hostname'] = os_info_dict.get('hostname')
-            os_info_data['OS Version'] = os_info_dict.get('OS Version').split('\n')[0]
-            os_info_data['CPU'] = parse_cpu_info(os_info_dict.get('cpu'))
-            os_info_data['Memory'] = os_info_dict.get('memory').split('\n')[0].replace('MemTotal:', '').strip()
-            os_info_data['my.cnf'] = os_info_dict.get('my.cnf')
-
-        except Exception as e:
-            self.logger.warning("An error occurred while parsing the OS info file...")
-            self.logger.warning(e)
-
-        all_data = {**status_data, **os_info_data, **variables_data}
-        all_data = {k.lower(): v for k, v in all_data.items()}
-
-        report = Document(self.sample_report_path)
-
-        for paragraph in report.paragraphs:
-            text = paragraph.text
-
-            # 이미지 삽입 처리
-            words = text.split()
-            for word in words:
-                if word.startswith("{graph_") and word.endswith("}"):
-                    self.insert_graph(self.graph_folder, paragraph, word)
-
-        # 표 내 데이터 변경
-        for table in report.tables:
-            for row in table.rows:
-                for cell in row.cells:
-                    text = cell.text.strip()
-
-                    # {graph_로 시작하면 이미지 삽입만 수행
-                    if text.startswith("{graph_") and text.endswith("}"):
-                        self.insert_graph(self.graph_folder, cell.paragraphs[0], text)
-
-                    # 그렇지 않으면 일반 텍스트 치환 수행
-                    elif text.startswith("{"):
-                        key = text.lstrip('{').rstrip('}').lower()
-
-                        if key in all_data.keys():
-                            cell.text = ""  # 기존 텍스트 삭제 (안 하면 스타일 초기화됨)
-                            para = cell.paragraphs[0]
-
-                            run = para.add_run(all_data.get(key))
-                            run.font.name = "Malgun Gothic"
-
-                            # 정렬 방식 지정
-                            if key in left_aligned_param_list:
-                                para.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-
-                            else:
-                                para.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-
-        # 변경된 문서 저장
-        report.save(self.report_path)
-
-        #######################################################################
-        # 3.3.	Variables 정보 - 표 삽입
-        self.logger.info("input variable table on diagnostic report")
-
-        report = Document(self.report_path)
-
-        table = find_table_after_heading(report, "Variables 정보")
-        if table is None:
-            print("해당 제목 아래에 표를 찾을 수 없습니다.")
-            return
-
-        while len(table.rows) > 1:
-            table._element.remove(table.rows[-1]._element)
-
-        # 데이터 삽입
-        for i, (key, value) in enumerate(variables_data.items()):
-            row_cells = table.add_row().cells  # 새로운 행 추가
-
-            row_cells[0].paragraphs[0].add_run(key).font.size = Pt(8)
-            row_cells[1].paragraphs[0].add_run(str(value)).font.size = Pt(8)
-
-            # 줄무늬 스타일 (홀수 행은 회색, 짝수 행은 흰색)
-            set_cell_shading(row_cells[0], "F2F2F2")
-
-        # 수정된 문서 저장
-        report.save(self.report_path)
+    #
+    # @Common.exception_handler
+    # def make_report(self):
+    #     # 설정값 + 상태값 + 그래프 삽입
+    #     self.logger.info("input status and graph on diagnostic report")
+    #     db = DBwork()
+    #     status_data = db.get_latest_status_data()
+    #     os_info_data = dict()
+    #     variables_data = dict()
+    #
+    #     # 왼쪽 정렬 파라미터 리스트 (여기 없으면 모두 가운데 정렬)
+    #     left_aligned_param_list = [
+    #         'my.cnf', 'hostname', 'memory', 'os version', 'cpu', 'version',
+    #         'log_error', 'slow_query_log_file', 'log_bin_basename', 'innodb_data_file_path',
+    #         'default_storage_engine', 'port', 'socket', 'basedir', 'datadir'
+    #     ]
+    #
+    #     os_info_dict = self.get_os_info()
+    #     variables_data = parse_table_to_dict(os_info_dict.get('global variables'))
+    #
+    #     try:
+    #         os_info_dict = self.get_os_info()
+    #         variables_data = parse_table_to_dict(os_info_dict.get('global variables'))
+    #
+    #         os_info_data['hostname'] = os_info_dict.get('hostname')
+    #         os_info_data['OS Version'] = os_info_dict.get('OS Version').split('\n')[0]
+    #         os_info_data['CPU'] = parse_cpu_info(os_info_dict.get('cpu'))
+    #         os_info_data['Memory'] = os_info_dict.get('memory').split('\n')[0].replace('MemTotal:', '').strip()
+    #         os_info_data['my.cnf'] = os_info_dict.get('my.cnf')
+    #
+    #     except Exception as e:
+    #         self.logger.warning("An error occurred while parsing the OS info file...")
+    #         self.logger.warning(e)
+    #
+    #     all_data = {**status_data, **os_info_data, **variables_data}
+    #     all_data = {k.lower(): v for k, v in all_data.items()}
+    #
+    #     report = Document(self.sample_report_path)
+    #
+    #     for paragraph in report.paragraphs:
+    #         text = paragraph.text
+    #
+    #         # 이미지 삽입 처리
+    #         words = text.split()
+    #         for word in words:
+    #             if word.startswith("{graph_") and word.endswith("}"):
+    #                 self.insert_graph(self.graph_folder, paragraph, word)
+    #
+    #     # 표 내 데이터 변경
+    #     for table in report.tables:
+    #         for row in table.rows:
+    #             for cell in row.cells:
+    #                 text = cell.text.strip()
+    #
+    #                 # {graph_로 시작하면 이미지 삽입만 수행
+    #                 if text.startswith("{graph_") and text.endswith("}"):
+    #                     self.insert_graph(self.graph_folder, cell.paragraphs[0], text)
+    #
+    #                 # 그렇지 않으면 일반 텍스트 치환 수행
+    #                 elif text.startswith("{"):
+    #                     key = text.lstrip('{').rstrip('}').lower()
+    #
+    #                     if key in all_data.keys():
+    #                         cell.text = ""  # 기존 텍스트 삭제 (안 하면 스타일 초기화됨)
+    #                         para = cell.paragraphs[0]
+    #
+    #                         run = para.add_run(all_data.get(key))
+    #                         run.font.name = "Malgun Gothic"
+    #
+    #                         # 정렬 방식 지정
+    #                         if key in left_aligned_param_list:
+    #                             para.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+    #
+    #                         else:
+    #                             para.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    #
+    #     # 변경된 문서 저장
+    #     report.save(self.report_path)
+    #
+    #     #######################################################################
+    #     # 3.3.	Variables 정보 - 표 삽입
+    #     self.logger.info("input variable table on diagnostic report")
+    #
+    #     report = Document(self.report_path)
+    #
+    #     table = find_table_after_heading(report, "Variables 정보")
+    #     if table is None:
+    #         print("해당 제목 아래에 표를 찾을 수 없습니다.")
+    #         return
+    #
+    #     while len(table.rows) > 1:
+    #         table._element.remove(table.rows[-1]._element)
+    #
+    #     # 데이터 삽입
+    #     for i, (key, value) in enumerate(variables_data.items()):
+    #         row_cells = table.add_row().cells  # 새로운 행 추가
+    #
+    #         row_cells[0].paragraphs[0].add_run(key).font.size = Pt(8)
+    #         row_cells[1].paragraphs[0].add_run(str(value)).font.size = Pt(8)
+    #
+    #         # 줄무늬 스타일 (홀수 행은 회색, 짝수 행은 흰색)
+    #         set_cell_shading(row_cells[0], "F2F2F2")
+    #
+    #     # 수정된 문서 저장
+    #     report.save(self.report_path)
 
     @Common.exception_handler
     def make_report_v2(self):
-        ################### test
-        self.sample_report_path = '../sample/sample_diagnostic_report_v1.3.docx'
-        self.report_path = './result/diagnostic_report.docx'
-        self.os_info_path = self.get_config()['path'].get('os_info_file')
-        self.graph_folder = './result/graphs/'
-        ###############################
+        # ################### test
+        # self.sample_report_path = '../sample/sample_diagnostic_report_v1.3.docx'
+        # self.report_path = './result/diagnostic_report.docx'
+        # self.os_info_path = self.get_config()['path'].get('os_info_file')
+        # self.graph_folder = './result/graphs/'
+        # ###############################
 
         # 설정값 + 상태값 + 그래프 삽입
         self.logger.info("input status and graph on diagnostic report")
@@ -447,7 +447,7 @@ class MSword(Common):
                         make_table_count(report, paragraph, engine_table_data, use_column=[1,1,1], header=['스키마 이름', '스토리지 엔진', '개수'])
 
                     elif word == '{_table_unused_user}':
-                        self.make_table_auto(report, paragraph, unused_user_data)
+                        self.make_table_auto(report, paragraph, unused_user_data, width=[0.2, 0.2])
 
                     elif word == '{_table_unused_index}':
                         self.make_table_auto(report, paragraph, unused_index_data, header=['스키마 이름', '테이블 이름', '인덱스 이름'])
@@ -462,13 +462,13 @@ class MSword(Common):
                         self.make_table_auto(report, paragraph, no_update_tables_data, header=['스키마 이름', '테이블 이름', '레코드 수', 'Count_read', 'Count_write'])
 
                     elif word == '{_table_fullscan_sql}':
-                        self.make_table_auto(report, paragraph, fullscan_sql_data, use_column=[1,1,0,0,0,0,0], header=['스키마 이름', 'SQL'])
+                        self.make_table_auto(report, paragraph, fullscan_sql_data, use_column=[1,1,0,0,0,0,0], header=['스키마 이름', 'SQL'], width=[0.2, 0.8])
 
                     elif word == '{_table_temptable_sql}':
-                        self.make_table_auto(report, paragraph, temptable_sql_data, use_column=[1,1,0,0,0], header=['스키마 이름', 'SQL'])
+                        self.make_table_auto(report, paragraph, temptable_sql_data, use_column=[1,1,0,0,0], header=['스키마 이름', 'SQL'], width=[0.2, 0.8])
 
                     elif word == '{_table_unconstrained_tables}':
-                        self.make_table_auto(report, paragraph, unconstrained_tables_data, header=['스키마 이름', '테이블 이름'])
+                        self.make_table_auto(report, paragraph, unconstrained_tables_data, header=['스키마 이름', '테이블 이름'], width=[0.15, 0.4])
 
         # 수정된 문서 저장
         report.save(self.report_path)
@@ -511,7 +511,7 @@ class MSword(Common):
 
     @staticmethod
     @Common.exception_handler
-    def make_table_auto(report, paragraph, data: list, use_column=[], header=[]):
+    def make_table_auto(report, paragraph, data: list, use_column=[], header=[], width=[]):
         if use_column:
             data = [
                 tuple(val for val, use in zip(row, use_column) if use == 1)
@@ -523,11 +523,17 @@ class MSword(Common):
         table.autofit = False
         paragraph._element.addnext(table._element)
 
+        total_width = 6.25
+        col_widths = [total_width * r for r in width]
+
         for row_idx, row_data in enumerate(data):
             for col_idx, value in enumerate(row_data):
 
                 cell = table.cell(row_idx, col_idx)
                 cell.text = str(value)
+
+                if width:
+                    cell.width = Inches(col_widths[col_idx])
 
                 if row_idx == 0:
                     if header:
